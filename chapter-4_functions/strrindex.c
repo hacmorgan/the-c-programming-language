9ac32630926b6cf20726d8865babf4e79cee39d5
hacmorgan <ham430@gmail.com>
2020-09-07 22:24:47 +1000

    strrindex

---
+++ chapter-4_functions/strrindex.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+/* int getline( char line[], int max ); */
+int strindex(  char source[], char searchfor[] );
+
+char pattern[] = "ould";
+
+
+/* strindex: return index of t in s, -1 if none */
+int strindex( char s[], char t[] )
+{
+    int i, j, k;
+
+    for ( i = 0 ; s[i] != '\0' ; i++ ) {
+        for ( j = i, k = 0 ; t[k] != '\0' && s[j] == t[k] ; j++, k++ ) {
+            ;
+        }
+        if ( k > 0 && t[k] == '\0' ) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+/* strindex: return index of rightmost occurence of t in s, -1 if none */
+int strrindex( char s[], char t[] )
+{
+    int i, j, k;
+
+    for ( i = strlen(s) - 1 ; s[i] >= 0 ; i-- ) {
+        for ( j = i, k = 0 ; t[k] != '\0' && s[j] == t[k] ; j++, k++ ) {
+            ;
+        }
+        if ( k > 0 && t[k] == '\0' ) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+/* get line into s, return length  */
+int getline( char s[], int lim )
+{
+	int c, i;
+
+	i = 0;
+	while ( --lim > 0 && (c=getchar()) != EOF && c != '\n' ) {
+		s[i++] = c;
+	}
+	if ( c == '\n' ) {
+		s[i++] = c;
+	}
+	s[i] = '\0';
+
+	return i;
+}
+
+
+/* find all lines matching pattern */
+int main ()
+{
+	char line[MAXLINE];
+	int found = 0;
+
+	while ( getline( line, MAXLINE ) > 0 ) {
+		if ( strrindex( line, pattern ) >= 0 ) {
+			printf( "%s", line );
+			found++;
+		}
+	}
+
+
+    char string[] = "would could should";
+    printf( "%d\n", strrindex( string, pattern ) );
+    
+	return found;
+}
