15235cd3430aff1462e9c6d2f32624dd7549969d
hacmorgan <ham430@gmail.com>
2020-09-07 22:23:27 +1000

    atof added

---
+++ chapter-4_functions/atof/atof.c
@@ -0,0 +1,46 @@
+#include <ctype.h>
+
+/* atof: convert string s to double */
+double atof( char s[] )
+{
+    double val, power, exponent, result;
+    int i, sign, exponentSign;
+
+    for ( i=0 ; isspace(s[i]) ; i++ ) {
+        ; /* skip whitespace */
+    }
+    
+    sign = ( s[i] == '-' ) ? -1 : 1;
+    if ( s[i] == '+' || s[i] == '-' ) {
+        i++;
+    }
+    
+    for ( val=0 ; isdigit(s[i]) ; i++ ) {
+        val = 10.0 * val + ( s[i] - '0' );
+    }
+    if ( s[i] == '.' ) {
+        i++;
+    }
+    for ( power=1.0 ; isdigit(s[i]) ; i++ ) {
+        val = 10.0 * val + ( s[i] - '0' );
+        power *= 10;
+    }
+    result = sign * val / power;
+    
+    if ( s[i] == 'e' || s[i] == 'E' ) {
+        exponentSign = ( s[++i] == '-' ) ? -1 : 1;
+        for ( exponent=0, ++i ; isdigit(s[i]) ; i++ ) {
+            exponent = 10 * exponent + ( s[i] - '0' );
+        }
+        while ( exponent < 0 ) {
+            result /= 10;
+            exponent++;
+        }
+        while ( exponent > 0 ) {
+            result *= 10;
+            exponent--;
+        }
+    }
+    return result;
+}
+
